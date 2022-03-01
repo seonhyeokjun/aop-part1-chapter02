0990b6c8343354d0840a85303c1fe1d0a61654a8
seonhyeokjun <tjsrmatn@naver.com>
2022-03-01 22:58:50 +0900

    Initial commit

---
+++ app/src/main/java/com/aop/part1/chapter02/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.aop.part1.chapter02
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aop.part1.chapter02.ui.theme.Aoppart1chapter02Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Aoppart1chapter02Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Aoppart1chapter02Theme {
+        Greeting("Android")
+    }
+}
